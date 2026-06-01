19a476dd91214b374ca65ed568661467b5f55ca7
khyatiydv21 <ykhyati678@gmail.com>
2026-06-01 21:56:18 +0530

    Implement function to remove duplicates from sorted array

---
+++ Week_1/Day_1/q-2-Remove_duplicate_fromsprted_array.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+      int j =0;
+      int k=1;
+      if(nums.empty()) return 0;
+      for(int i=1; i<nums.size(); i++){
+        if(nums[i]!=nums[i-1]){
+            j++;
+ nums[j] = nums[i];
+
+k++;
+        
+      }
+      
+      }
+      return k;
+    }
+
+    int main(){
+        vector<int>nums = {0,0,1,1,1,2,2,3,3,4};
+        
+ cout<< removeDuplicates(nums);
+    }
